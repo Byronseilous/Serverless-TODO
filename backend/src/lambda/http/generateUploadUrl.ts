e532c69642130216c2b0717dd8e9a3db59fbbd00
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2022-06-08 17:45:56 +0300

    chore: Update generateUploadUrl.ts (generateUploadUrl.ts)

---
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -0,0 +1,41 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { createLogger } from '../../utils/logger'
+import { getUserId } from '../utils'
+import { getUploadUrl, todoExists } from '../../businessLogic/todos'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  createLogger(`Processing delete todo event: ${event}`)
+
+  const userId = getUserId(event)
+  const todoId = event.pathParameters.todoId
+  const validTodo = await todoExists(todoId, userId)
+
+  if (!validTodo) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
+
+  const url = await getUploadUrl(todoId, userId)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  }
+}
+
