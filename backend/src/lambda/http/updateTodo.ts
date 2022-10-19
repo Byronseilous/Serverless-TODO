7cf863c2bbe602ef68c9cc628ed5ae06f7c70f18
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2022-10-19 16:46:36 +0300

    chore: Update updateTodo.ts (updateTodo.ts)

---
+++ backend/src/lambda/http/updateTodo.ts
@@ -0,0 +1,42 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { createLogger } from '../../utils/logger'
+import { getUserId } from '../utils'
+import { updateTodo, todoExists } from '../../businessLogic/todos'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  createLogger(`Processing update todos event: ${event}`)
+
+  const userId = getUserId(event)
+  const todoId = event.pathParameters.todoId
+  const validTodo = await todoExists(todoId, userId)
+
+  if (!validTodo) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
+
+  const payload: UpdateTodoRequest = JSON.parse(event.body)
+
+  await updateTodo(todoId, userId, payload)
+
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({})
+  }
+}
