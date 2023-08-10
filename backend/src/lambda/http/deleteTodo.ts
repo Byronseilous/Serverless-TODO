d92c4af3b8c2ae1643e70debfddfa911de5b035f
Byronseilous <104057957+Byronseilous@users.noreply.github.com>
2023-08-10 07:49:04 +0300

    chore: Update deleteTodo.ts (deleteTodo.ts)

---
+++ backend/src/lambda/http/deleteTodo.ts
@@ -0,0 +1,40 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+
+import { createLogger } from '../../utils/logger'
+import { getUserId } from '../utils'
+import { deleteTodo, todoExists } from '../../businessLogic/todos'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  createLogger(`Processing delete todo event: ${event}`)
+
+  const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
+  const validTodo = await todoExists(todoId, userId)
+
+  if (!validTodo) {
+    return {
+      statusCode: 404,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        error: 'Todo does not exist'
+      })
+    }
+  }
+
+  await deleteTodo(todoId, userId)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({})
+  }
+}
+
